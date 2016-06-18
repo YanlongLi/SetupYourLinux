fe76562e49622fd9d4991370cfd7001cad9968e3
Yanlong Li <lansunlong@gmail.com>
2016-06-18 22:16:02 +0800

    init_zsh.sh

---
+++ init_zsh.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+plugindir=~/.oh-my-zsh/plugins
+baseuri=https://github.com
+plugins=(zsh-users/zsh-autosuggestions zsh-users/zsh-syntax-highlighting)
+profiles=(zshrc zprofile)
+
+cur=`pwd`
+gitdir=${1-`realpath ./`}
+
+if [ ! -d $plugindir ]; then
+  echo "$plugindir not exist, create it"
+  mkdir -p $plugindir
+fi
+
+echo "change directory to $plugindir ..."
+cd $plugindir
+for path in ${plugins[@]}; do
+  echo "clone plugin $path begin ..."
+  git clone $baseuri/$path.git >/dev/null 2>&1
+done
+
+cd $cur
+
+echo "copy zsh profiles"
+for fname in ${profiles[@]}; do
+  if [ -f ~/zshrc ]; then
+    mv -f ~/.zshrc ~/.zshrc.back
+  fi
+  cp -f $gitdir/home/$fname ~/.$fname
+done
+echo "use command below to change your default shell:"
+echo "sudo chsh -s $(which zsh) \$USER"
+echo "~~~~~~~~~~~~ done ~~~~~~~~~~~~~"
+
