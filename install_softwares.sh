dbbc746d6c5d77f8132687f29668c18233451abd
Yanlong Li <lansunlong@gmail.com>
2017-10-12 12:12:41 +0000

    install softwares

---
+++ install_softwares.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+sudo apt-get install --assume-yes python-dev
+sudo apt-get install --assume-yes python3-dev
+sudo apt-get install --assume-yes python-setuptools
+sudo apt-get install --assume-yes python-pip
+sudo apt-get install --assume-yes node
+sudo apt-get install --assume-yes nodejs-legacy
+sudo apt-get install --assume-yes npm
+sudo apt-get install --assume-yes tmux
+sudo apt-get install --assume-yes ssh
+sudo apt-get install --assume-yes openssh-client
+sudo apt-get install --assume-yes zsh
+sudo apt-get install --assume-yes cifs-utils
+sudo apt-get install --assume-yes samba
+sudo apt-get install --assume-yes texlive-full
+sudo apt-get install --assume-yes gvim
+sudo apt-get install --assume-yes clang
+sudo apt-get install --assume-yes llvm
+sudo apt-get install --assume-yes libboost-all-dev
+sudo apt-get install --assume-yes mono-xbuild
+sudo apt-get install --assume-yes mono-dmcs
+sudo apt-get install --assume-yes cmake
+
+sudo apt-get install --assume-yes tidy astyle htop thefuck
+
+sudo npm install -g js-beautify remark
+
+sudo pip2 install autopep8 yapf
+sudo pip2 install jupyter
+sudo apt-get install --assume-yes python-numpy python-matplotlib python-scipy
+
