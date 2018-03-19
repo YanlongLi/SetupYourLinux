38905cd19c663534a09ea3e2a0a5f451f650351f
YanlongLi <lansunlong@gmail.com>
2018-03-19 11:52:21 +0000

    Fix

--- init_zsh.sh
+++ init_zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 plugindir=~/.oh-my-zsh/plugins
 baseuri=https://github.com
